fb1044dc6a3fc7624185369e0c14b806985e6d5a
Jesper <jespervanemden@gmail.com>
2024-07-18 10:43:15 +0200

    Added multiple indicators

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,10 @@ import io
 import time
 import random
 import openpyxl
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+import base64
+from Strategies.buy_and_hold import BuyAndHold
 
 # Define folder paths
 TICKERS_CSV_PATH = './Tickers/tickers.csv'
@@ -29,27 +33,28 @@ def load_strategies():
                     strategies[name] = obj
     return strategies
 
-# Function to run backtest
 def run_backtest(data, strategy_class, start_cash=10000.0, commission=0.001):
     cerebro = bt.Cerebro()
     cerebro.adddata(data)
     cerebro.addstrategy(strategy_class)
     cerebro.broker.setcash(start_cash)
     cerebro.broker.setcommission(commission=commission)
-    strategies = cerebro.run()
+    strategies = cerebro.run(runonce=False)
     final_value = cerebro.broker.getvalue()
     strategy = strategies[0]
     trade_count = strategy.order_count if hasattr(strategy, 'order_count') else 0
     current_signal = strategy.signal if hasattr(strategy, 'signal') else None
-    return final_value, trade_count, current_signal
+    roi = strategy.roi if hasattr(strategy, 'roi') else ((final_value / start_cash) - 1.0)
+    return final_value, trade_count, current_signal, roi
 
-# Function to run buy and hold
-def buy_and_hold(data, start_cash=10000.0):
+#Function to compare the strategy to buy and hold
+def calculate_buy_and_hold(data, start_cash=10000.0):
     initial_price = data['Close'].iloc[0]
     final_price = data['Close'].iloc[-1]
     shares = start_cash / initial_price
     final_value = shares * final_price
     return final_value
+
 
 def fetch_data_with_retry(ticker, start_date, end_date, max_retries=5):
     for attempt in range(max_retries):
@@ -96,27 +101,38 @@ for index, row in tickers_df.iterrows():
         all_start_dates.append(df.index[0])
         all_end_dates.append(df.index[-1])
         data = bt.feeds.PandasData(dataname=df)
+
+        # Calculate init and latest price to give a feeling of stock movement
+        initial_price = df['Close'].iloc[0]
+        final_price = df['Close'].iloc[-1]
+
+        # Run Buy and Hold strategy
+        bh_final_value, bh_trade_count, bh_signal, bh_roi = run_backtest(data, BuyAndHold, start_cash, commission)
+        bh_profit = bh_final_value - start_cash
+        bh_profit_percentage = bh_roi * 100
         
         for strat_name, strategy_class in all_strategies.items():
             try:
-                final_value, trade_count, current_signal = run_backtest(data, strategy_class, start_cash, commission)
+                final_value, trade_count, current_signal, roi = run_backtest(data, strategy_class, start_cash, commission)
                 profit = final_value - start_cash
-                profit_percentage = (profit / start_cash) * 100
-                buy_and_hold_value = buy_and_hold(df, start_cash)
-                buy_and_hold_profit = buy_and_hold_value - start_cash
-                diff_to_buy_and_hold = profit - buy_and_hold_profit
+                profit_percentage = roi * 100
+                
+                # Calculate the difference from Buy and Hold
+                profit_corrected = profit_percentage - bh_profit_percentage
                 
                 results.append({
                     'Ticker': ticker,
                     'Name': name,
+                    'Initial Price': initial_price,
+                    'Final Price': final_price,
                     'Strategy': strat_name,
                     'Final Value (EUR)': round(final_value, 2),
                     'Profit (EUR)': round(profit, 2),
                     'Profit (%)': round(profit_percentage, 2),
+                    'Profit_corrected for B&H (%)': round(profit_corrected, 2),
                     'Start Date': df.index[0].strftime('%Y-%m-%d'),
                     'End Date': df.index[-1].strftime('%Y-%m-%d'),
                     'Trades': trade_count,
-                    'Difference to Buy & Hold (EUR)': round(diff_to_buy_and_hold, 2),
                     'Buy/Sell Signal': current_signal
                 })
             except Exception as e:
